82d00b2a21c169da83c7c591facaaebeaf15732e
Kota@mac <miura@embl.de>
2011-07-22 02:18:20 +0200

    test js for stack size adjustment done

--- testfunc.js
+++ testfunc.js
@@ -1,8 +1,15 @@
+// test for adjusting destination stack size
+
 var mpimodel = new JavaImporter();
 mpimodel.importPackage(Packages.mpicbg.models);
+var trakmodel = new JavaImporter();
+trakmodel.importPackage(Packages.mpicbg.trakem2.transform);
+
 importPackage(Packages.java.util);
 var mpistack = new JavaImporter();
 mpistack.importPackage(Packages.mpicbg.ij.stack);
+var jarray = new JavaImporter();
+jarray.importPackage(Packages.java.lang.reflect);
 
 pointPairs = new ArrayList();
 p11 = [1, 0, 0];
@@ -25,32 +32,38 @@ pointPairs.add(mpimodel.PointMatch(mpimodel.Point(p41), mpimodel.Point(p42)));
 IJ.log(pointPairs.toString());
 
 //LS fitting
-modelM = new mpimodel.AffineModel3D();
+modelM = new trakmodel.AffineModel3D();
 modelM.fit( pointPairs );
+IJ.log(modelM.toDataString());
 
 imp = IJ.getImage();
 ww = imp.getWidth();
 hh = imp.getHeight();
 dd = imp.getImageStackSize();
-//pixels = new java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, width * height);
-minA = [0, 0, 0];
-maxA = [ww, hh, dd]; 
-destsizeA = ReCalcStackSize(modelM, minA, maxA);
-//modelM.estimateBounds(minA, maxA);
-//newsizeA = [0.0, 0.0, 0.0]; 
-/*
-IJ.log("original:"+ ww.toString() +"," + hh.toString()+ "," + dd.toString()); 
-for (i in minA)
-  IJ.log(minA[i]);
-for (i in maxA)
-  IJ.log(maxA[i]);
-for (i in destsizeA) 
-  destsizeA[i] = maxA[i] - minA[i];  
-*/
-//newsizeA[i] = Math.ceil(maxA[i] - minA[i]);  
-for (var i = 0; i < minA.length; i++)
-  IJ.log(destsizeA[i]);
 
+// need to use java native array for estimateBounds() method.
+minA = jarray.Array.newInstance(java.lang.Float.TYPE, 3);
+maxA = jarray.Array.newInstance(java.lang.Float.TYPE, 3);
+
+minA[0] = 0.0;
+minA[1] = 0.0;
+minA[2] = 0.0;
+maxA[0] = ww;
+maxA[1] = hh;
+maxA[2] = dd;
+
+destsizeA = ReCalcStackSize(modelM, minA, maxA);
+
+IJ.log("original:"+ ww.toString() +"," + hh.toString()+ "," + dd.toString()); 
+for (i in minA)IJ.log(minA[i]);
+for (i in maxA)IJ.log(maxA[i]);
+outstr = "after: ";
+for (var i = 0; i < minA.length; i++)
+  outstr = outstr + destsizeA[i] + ",";
+IJ.log(outstr);
+
+ReCalcOffset(modelM, minA);
+IJ.log(modelM.toDataString());
 
 function ReCalcStackSize(transM, minA, maxA){
   transM.estimateBounds(minA, maxA);
