31c2d061a9195145fac322850f28da53142944c3
Kota Miura <miura@embl.de>
2011-08-24 18:06:12 +0200

    levelset ext update

--- LevelSetsEXT.js
+++ LevelSetsEXT.js
@@ -5,26 +5,30 @@
 // this file should be placed in the Fiji plugins folder.  
 
 importClass(Packages.emblcmci.tools.LevelSetExt);
-imp = IJ.getImage();
-
+imp1 = IJ.getImage();
+imp2 = doLevelSet(imp1)
+imp2.show();
+function doLevelSet(imp){
 //set parameters
-useFastMarching = false;
-useLevelSets = true;
-greyValueThreshold = 4.0;  
-distanceThreshold = 1.0;
-advection = 2.20;
-curvature = 1; 
-propagation = 1;
-grayscale = 30;
-convergence = 0.0050;
-inwards = false;
+	var useFastMarching = false;
+	var useLevelSets = true;
+	var greyValueThreshold = 4.0;  
+	var distanceThreshold = 1.0;
+	var advection = 40;
+	var curvature = 2; 
+	var propagation = 1;
+	var grayscale = 30;
+	var convergence = 0.0050;
+	var inwards = false;
 // parameter setting finished
 
 //constructor of LevelSets with parameters
-ls = new LevelSetExt(useFastMarching, useLevelSets, 
-	distanceThreshold, greyValueThreshold, 
-	advection, curvature, propagation, grayscale, convergence, 
-	inwards);
+	var ls = new LevelSetExt(useFastMarching, useLevelSets, 
+		distanceThreshold, greyValueThreshold, 
+		advection, curvature, propagation, grayscale, convergence, 
+		inwards);
 //execution with current image imp
-impout = ls.execute(imp, false); //arg[1] = false suppresses the display of progress image window
-impout.show();
+	impout = ls.execute(imp, true); //arg[1] = false suppresses the display of progress image window
+	return impout;
+}
+
