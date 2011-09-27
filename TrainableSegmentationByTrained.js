e5ba25c31784b256d0a2c8b3561bd4638e7fdc41
Kota Miura <miura@embl.de>
2011-09-27 17:45:46 +0200

    trainable segmentation changed to command line arguments for path

--- TrainableSegmentationByTrained.js
+++ TrainableSegmentationByTrained.js
@@ -4,13 +4,24 @@
 // 20110819
 // Kota Miura (miura@embl.de)
 
-// get currently active image / image stack
-imp = IJ.getImage();
+// argument = path to the image
+
+//imp = IJ.getImage();
+args = getArgument();
+argA = Tools.split(args, ":");
+IJ.log("source stack: " + argA[0]);
+IJ.log("source trained data: " + argA[1]);
+filepath = argA[0];
+imp = IJ.openImage(filepath);
 
 //path to the arff data
-traindata = "D:\\People\\Tina\\20110813\\data02.arff";
+//traindata = "D:\\People\\Tina\\20110813\\data02.arff";
+//traindata = "/g/cmci/likun/e1cell1/data.arff";
+traindata = argA[1];
 
 importClass(Packages.emblcmci.DotSegmentByTrained);
 dbt = new DotSegmentByTrained(traindata, imp);
 binimp = dbt.runsilent();
-binimp.show();
+//binimp.show();
+newfilename = filepath + ".train.tif"
+IJ.saveAs(binimp, "Tiff", newfilename); 
